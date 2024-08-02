2753f095a34646fa3b47fd91d777b6429e07d41a
Pelirosa <kargabrysia@gmail.com>
2024-08-02 16:24:43 +0200

    added initial files

--- scene/dusman.gd
+++ scene/dusman.gd
@@ -4,22 +4,24 @@ extends CharacterBody2D
 const SPEED = 170.0
 const JUMP_VELOCITY = -315.0
 var health = 3
-
+var alive = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var sprite = $AnimatedSprite2D
 @onready var dmg_animation = $flash
+@onready var dmg_animation1 = $visible
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if alive:
+			# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		if Input.is_action_just_pressed("up") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -40,9 +42,26 @@ func _physics_process(delta):
 	move_and_slide()
 
 
+
 func player_hit():
 	health -= 1 
 	dmg_animation.play("flash")
+	dmg_animation1.play("visible")
 	Global.health -=1
 	if health == 0:
 		queue_free()
+		#alive = false
+		#sprite.visible = false
+		#Global.health = 0
+
+
+func _on_area_2d_body_entered(body):
+	health -= 1 
+	dmg_animation.play("flash")
+	dmg_animation1.play("visible")
+	Global.health -=1
+	if health == 0:
+		#alive = false
+		queue_free()
+		#sprite.visible = false
+		#Global.health = 0
